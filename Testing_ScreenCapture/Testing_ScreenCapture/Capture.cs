3cf048ccc1e0a2d06c0e7ef6b2aeb3d81aa309e6
Akshay Kotish <akshaykotish@gmail.com>
2020-07-31 11:17:44 +0530

    Screenshot of windows using Win32 API

---
+++ Testing_ScreenCapture/Testing_ScreenCapture/Capture.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_ScreenCapture
+{
+    class Capture
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        public void capture(Process proc)
+        {
+            //Process proc = new Process();
+            //proc.StartInfo.FileName = "notepad.exe";
+            //proc.Start();
+            proc.WaitForInputIdle();
+            if (SetForegroundWindow(proc.MainWindowHandle))
+            {
+                RECT srcRect;
+                if (!proc.MainWindowHandle.Equals(IntPtr.Zero))
+                {
+                    if (GetWindowRect(proc.MainWindowHandle, out srcRect))
+                    {
+                        int width = srcRect.Right - srcRect.Left;
+                        int height = srcRect.Bottom - srcRect.Top;
+                        if (width != 0 && height != 0)
+                        {
+                            Bitmap bmp = new Bitmap(width, height);
+                            Graphics screenG = Graphics.FromImage(bmp);
+
+                            try
+                            {
+                                screenG.CopyFromScreen(srcRect.Left, srcRect.Top,
+                                        0, 0, new Size(width, height),
+                                        CopyPixelOperation.SourceCopy);
+
+                                bmp.Save(@"C:\Images\" + proc.ProcessName + ".jpg", ImageFormat.Jpeg);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            finally
+                            {
+                                screenG.Dispose();
+                                bmp.Dispose();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Testing_ScreenCapture/Testing_ScreenCapture/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_ScreenCapture
+{
+    public partial class Form1 : Form
+    {
+        Capture capture = new Capture();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var image = ScreenCapture.CaptureDesktop();
+            image.Save(@"C:\Images\s1.jpg", ImageFormat.Jpeg);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process[] procs = Process.GetProcesses();
+            IntPtr hWnd;
+            int i = 1;
+            foreach (Process proc in procs)
+            {
+                if ((hWnd = proc.MainWindowHandle) != IntPtr.Zero)
+                {
+                    Console.WriteLine("{0} : {1}", proc.ProcessName, hWnd);
+                    capture.capture(proc);
+                    //var img = ScreenCapture.CaptureWindow(proc.MainWindowHandle);
+                    //if(img != null)
+                    //{
+                    //    img.Save(@"C:\Images\" + proc.ProcessName + ".jpg", ImageFormat.Jpeg);
+                    //}
+
+                    //i++;
+                }
+            }
+        }
+    }
+}
